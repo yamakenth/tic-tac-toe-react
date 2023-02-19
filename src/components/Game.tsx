5ad03a9c0ad155323d0300a6563522ba020a4d73
Ken Yamada <yamakenth@gmail.com>
2023-02-18 20:51:07 -0500

    Lay out components for modal

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -15,9 +15,41 @@ export default function Game() {
 
   return (
     <div className="game-container">
+      {status !== "PENDING" && (
+        <div className="modal-container">
+          <div className="modal">
+            <div className="status-container">
+              <div className="modal-score">
+                <p>Player1</p>
+                <p>0</p>
+              </div>
+              <div className="modal-score">
+                <p>Games</p>
+                <p>0</p>
+              </div>
+              <div className="modal-score">
+                <p>Player2</p>
+                <p>0</p>
+              </div>
+            </div>
+            <p>
+              Do you want to play
+              <br />
+              ANOTHER ROUND or RESTART?
+            </p>
+            <div className="button-container">
+              <button type="button" className="another-round">
+                ANOTHER ROUND
+              </button>
+              <button type="button" className="restart">
+                RESTART
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
       <Board moves={moves} setMoves={setMoves} />
       <Scores moves={moves} />
-      <p>{status}</p>
     </div>
   );
 }
